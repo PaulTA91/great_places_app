c418c835c30e9ca169998af96398dea89e7b2917
PaulTA91 <parmst202@caledonian.ac.uk>
2022-09-28 12:20:25 +0100

    Lesson 307: Displaying static map snapshot

--- lib/widgets/location_input.dart
+++ lib/widgets/location_input.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:location/location.dart';
 
+import '../helpers/location_helper.dart';
+
 class LocationInput extends StatefulWidget {
   @override
   State<LocationInput> createState() => _LocationInputState();
@@ -11,8 +13,13 @@ class _LocationInputState extends State<LocationInput> {
 
   Future<void> _getUserLocation() async {
     final locationData = await Location().getLocation();
-    print(locationData.latitude);
-    print(locationData.longitude);
+    final previewURL = LocationHelper.generateLocationPreviewImage(
+      latitude: locationData.latitude,
+      longitude: locationData.longitude,
+    );
+    setState(() {
+      _previewImageURL = previewURL;
+    });
   }
 
   @override
